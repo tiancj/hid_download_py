2bac9cf52e4fea00e4b89421181f9443acf90991
tiancj <chunjian.tian@bekencorp.com>
2019-12-11 11:39:33 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+setup(name="hid_downloader",
+    version="0.1",
+    description="HID Download tool",
+    author="tiancj",
+    author_email="cj.tian.seu@gmail.com",
+    maintainer="tiancj",
+    maintainer_email="cj.tian.seu@gmail.com",
+    url="https://github.com/tiancj",
+    license="MIT",
+    requires=["hid"],
+    platforms=["Windows", "GNU/Linux", "Darwin"],
+    packages=['bkutils', 'bkutils/chip'],
+    scripts=['bekenprogram.py'])
+
