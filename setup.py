c6f8acc0c901f72d42e6fb2d118fc2ee7c20c840
tiancj <chunjian.tian@bekencorp.com>
2020-04-28 15:45:03 +0800

    bump to version 0.99

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from distutils.core import setup
 
 setup(name="hid_downloader",
-    version="0.1",
+    version="0.99",
     description="HID Download tool",
     author="tiancj",
     author_email="cj.tian.seu@gmail.com",
