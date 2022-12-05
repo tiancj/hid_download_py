8401821f6a313c3285331be33a993acf89def278
tiancj <chunjian.tian@bekencorp.com>
2022-12-05 18:03:13 +0800

    add more flash ids

--- bkutils/flash_list.py
+++ bkutils/flash_list.py
@@ -3,9 +3,9 @@
 FLASH_ID_XTX_25F08B=0x14405e        # 芯天下flash,w+v:6.5s,e+w+v:11.5s
 FLASH_ID_XTX_25F04B=0x13311c        # 芯天下flash-4M
 FLASH_ID_XTX_25F16B=0x15400b        # XTX 16M
-FLASH_ID_XTX_25F32B	=0x0016400b     # xtx 32M******暂时只用于脱机烧录器上7231的外挂flash
-FLASH_ID_XTX_25Q64B	=0x0017600b     # xtx 64M******暂时只用于脱机烧录器上7231的外挂flash
-FLASH_ID_XTX_25F64B	=0x0017400b     # xtx 64M******暂时只用于脱机烧录器上7231的外挂flash
+FLASH_ID_XTX_25F32B=0x0016400b      # xtx 32M******暂时只用于脱机烧录器上7231的外挂flash
+FLASH_ID_XTX_25Q64B=0x0017600b      # xtx 64M******暂时只用于脱机烧录器上7231的外挂flash
+FLASH_ID_XTX_25F64B=0x0017400b      # xtx 64M******暂时只用于脱机烧录器上7231的外挂flash
 FLASH_ID_MXIC_25V8035F=0x1423c2     # 旺宏flash,w+v:8.2s,e+w+v:17.2s
 FLASH_ID_MXIC_25V4035F=0x1323c2     # 旺宏flash,w+v:8.2s,e+w+v:17.2s
 FLASH_ID_MXIC_25V1635F=0x1523c2     # 旺宏flash,w+v:8.2s,e+w+v:17.2s
@@ -20,9 +20,11 @@ FLASH_ID_GD_25Q16=0x001540c8        # GD 16M******暂时只用于脱机烧录器
 FLASH_ID_GD_25Q16B=0x001540c8       # GD 16M******暂时只用于脱机烧录器上7231的外挂flash
 FLASH_ID_GD_25Q41B=0x1340c8         # GD flash-4M,w+v:3.1s,e+w+v:5.1s
 
+FLASH_ID_Puya_25Q16HB_K=0x152085
 FLASH_ID_Puya_25Q40=0x136085        # puya 4M,e+w+v:6s，新版w+v=4s，e+w+v=4.3s
 FLASH_ID_Puya_25Q64H=0x00176085
 FLASH_ID_Puya_25Q80=0x146085        # puya 8M,w+v:10.4s,e+w+v:11.3s,新版e+w+v：8.3s
+FLASH_ID_Puya_25Q80_38=0x154285
 FLASH_ID_Puya_25Q32H=0x166085       # puya 32M******暂时只用于脱机烧录器上7231的外挂flash
 FLASH_ID_BY_PN25Q80A=0x1440e0       # GD flash-4M,w+v:3.1s,e+w+v:5.1s
 FLASH_ID_BY_PN25Q40A=0x1340e0       # GD flash-4M,w+v:3.1s,e+w+v:5.1s
@@ -30,6 +32,7 @@ FLASH_ID_BY_PN25Q40A=0x1340e0       # GD flash-4M,w+v:3.1s,e+w+v:5.1s
 FLASH_ID_WB_25Q128JV=0x001840ef
 FLASH_ID_ESMT_25QH16B=0x0015701c
 FLASH_ID_ESMT_25QH32A=0x0016411c
+FLASH_ID_ESMT_25QW32A=0x0016611c
 FLASH_ID_TH25Q_16HB = 0x001560eb
 FLASH_ID_TH25Q_80HB = 0x001460cd
 FLASH_ID_NA = 0x001640c8
@@ -69,6 +72,8 @@ tblFlashInt = [
     FlashInt(FLASH_ID_GD_25D80,       "GD25D80",      "GD",       8 *1024*1024,   1,    0x00,    0x07,   BFD(0x0f,2,3),           2,    3,    [0x05,0xff,0xff,0xff],   [0x01,0xff,0xff,0xff]),
     FlashInt(FLASH_ID_GD_1_25D80,     "GD25D80",      "GD",       8 *1024*1024,   2,    0x00,    0x07,   BIT(14)|BFD(0x1f,2,5),   2,    5,    [0x05,0x35,0xff,0xff],   [0x01,0xff,0xff,0xff]),
     FlashInt(FLASH_ID_Puya_25Q80,     "P25Q80",       "Puya",     8 *1024*1024,   2,    0x00,    0x07,   BIT(14)|BFD(0x1f,2,5),   2,    5,    [0x05,0x35,0xff,0xff],   [0x01,0xff,0xff,0xff]),
+    FlashInt(FLASH_ID_Puya_25Q80_38,  "P25Q80",       "Puya",    16 *1024*1024,   1,    0x00,    0x07,   BIT(14)|BFD(0x1f,2,5),   2,    5,    [0x05,0x35,0xff,0xff],   [0x01,0xff,0xff,0xff]),
+    FlashInt(FLASH_ID_Puya_25Q16HB_K, "P25Q16HB_K",   "Puya",    16 *1024*1024,   2,    0x00,    0x07,   BIT(14)|BFD(0x1f,2,5),   2,    5,    [0x05,0x35,0xff,0xff],   [0x01,0xff,0xff,0xff]),
     FlashInt(FLASH_ID_Puya_25Q40,     "P25Q40",       "Puya",     4 *1024*1024,   2,    0x00,    0x07,   BIT(14)|BFD(0x1f,2,5),   2,    5,    [0x05,0x35,0xff,0xff],   [0x01,0xff,0xff,0xff]),
     FlashInt(FLASH_ID_Puya_25Q32H,    "P25Q32H",      "Puya",    32 *1024*1024,   2,    0x00,    0x07,   BIT(14)|BFD(0x1f,2,5),   2,    5,    [0x05,0x35,0xff,0xff],   [0x01,0xff,0xff,0xff]),
     FlashInt(FLASH_ID_Puya_25Q64H,    "P25Q64H",      "Puya",    64 *1024*1024,   2,    0x00,    0x07,   BIT(14)|BFD(0x1f,2,5),   2,    5,    [0x05,0x35,0xff,0xff],   [0x01,0xff,0xff,0xff]),
@@ -85,6 +90,7 @@ tblFlashInt = [
     FlashInt(FLASH_ID_WB_25Q128JV,    "WB25Q128JV",   "WB",     128 *1024*1024,   2,    0x00,    0x07,   BIT(14)|BFD(0x1f,2,5),   2,    5,    [0x05,0x35,0xff,0xff],   [0x01,0xff,0xff,0xff]),
     FlashInt(FLASH_ID_ESMT_25QH16B,   "EN25QH16B",    "ESMT",    16 *1024*1024,   1,    0x00,    0x07,   BFD(0xf,2,5),            2,    4,    [0x05,0xff,0xff,0xff],   [0x01,0xff,0xff,0xff]),
     FlashInt(FLASH_ID_ESMT_25QH32A,   "EN25QH32A",    "ESMT",    32 *1024*1024,   1,    0x00,    0x07,   BFD(0xf,2,5),            2,    4,    [0x05,0xff,0xff,0xff],   [0x01,0xff,0xff,0xff]),
+    FlashInt(FLASH_ID_ESMT_25QW32A,   "EN25QH32A",    "ESMT",    32 *1024*1024,   2,    0x00,    0x07,   BIT(14)|BFD(0x1f,2,5),   2,    5,    [0x05,0x35,0xff,0xff],   [0x01,0xff,0xff,0xff]),
     FlashInt(FLASH_ID_TH25Q_16HB,     "TH25Q_16HB",   "TH",      16 *1024*1024,   2,    0x00,    0x07,   BIT(14)|BFD(0x1f,2,5),   2,    5,    [0x05,0x35,0xff,0xff],   [0x01,0xff,0xff,0xff]),
     FlashInt(FLASH_ID_TH25Q_80HB,     "TH25Q_80HB",   "TH",       8 *1024*1024,   2,    0x00,    0x07,   BIT(14)|BFD(0x1f,2,5),   2,    5,    [0x05,0x35,0xff,0xff],   [0x01,0xff,0xff,0xff]),
     FlashInt(FLASH_ID_NA,             "NA_NA",        "NA",      32 *1024*1024,   1,    0x00,    0x07,   BIT(14)|BFD(0x1f,2,5),   2,    5,    [0x05,0x35,0xff,0xff],   [0x01,0xff,0xff,0xff]),
